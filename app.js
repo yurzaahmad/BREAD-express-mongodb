aad7926e09fc254b44ace08b5ea3c857b7d6ea17
yurza <ahmadyurza@gmail.com>
2020-08-22 23:19:58 +0700

    clear

--- app.js
+++ app.js
@@ -1,41 +1,61 @@
-var createError = require('http-errors');
 var express = require('express');
+var createError = require('http-errors');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
 
 var app = express();
 
-// view engine setup
+
+var usersRouter = require('./routes/users');
+
+
+// Use connect method to connect to the server
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
+
+// view engine setup
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+// Database Name
+const dbName = 'tabeldb';
+
+MongoClient.connect(url, function (err, client) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+  const indexRouter = require('./routes/index')(db);
+  app.use('/', indexRouter);
+  // catch 404 and forward to error handler
+  app.use(function (req, res, next) {
+    next(createError(404));
+  });
+
+  // error handler
+  app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,130 @@
 var express = require('express');
+const { ObjectID } = require('mongodb');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+module.exports = (db) => {
+  router.get('/', function (req, res, next) {
+    const { checkId, id, checkString, string, checkInteger, integer, checkFloat, float, checkBoolean, boolean, checkDate, startDate, endDate } = req.query;
+    let query = new Object();
+    const reg = new RegExp(string);
 
-module.exports = router;
+    if (checkId && id) {
+      query._id = id;
+    }
+    if (checkString && string) {
+      query.string = reg;
+    }
+    if (checkInteger && integer) {
+
+      console.log('disini')
+      query.integer = parseInt(integer);
+    }
+    if (checkFloat && float) {
+      query.float = parseFloat(float);
+    }
+    if (checkBoolean && boolean) {
+      query.boolean = JSON.parse(bool);
+    }
+    if (checkDate && startDate && endDate) {
+      query.date = { $gte: startDate, $lte: endDate }
+    }
+    const page = req.query.page || 1;
+    const limit = 3;
+
+    const offset = (page - 1) * limit;
+
+    //New Page by Pagination
+    let url = req.url.includes('page') ? req.url : `/?page=1&` + req.url.slice(2)
+    console.log(query)
+    db.collection('siswa').count()
+      .then((total) => {
+        const pages = Math.ceil(total / limit)
+
+        db.collection('siswa').find(query).limit(limit).skip(offset).toArray((err, data) => {
+          if (err) return res.json(err)
+          console.log(data);
+
+          res.render('index', {
+            data,
+            page,
+            pages,
+            url
+          })
+
+        })
+      }).catch(err => console.log(err))
+
+
+
+  });
+  router.get('/add', (req, res) => res.render('add'))
+
+  router.post('/add', (req, res) => {
+    let insert = req.body;
+    let add = {
+      "string": insert.string,
+      "integer": insert.integer,
+      "float": insert.float,
+      "date": insert.date,
+      "boolean": insert.boolean
+    }
+    db.collection('siswa').insertOne(add, err => {
+      if (err) res.json(err)
+    })
+    res.redirect('/')
+  });
+
+  router.get('/delete/:id', (req, res) => {
+    console.log('wkwkw');
+    let id = req.params.id
+    console.log(id);
+    db.collection('siswa').deleteOne({
+      "_id": ObjectID(id)
+    }, err => {
+      if (err) res.json(err)
+    })
+    res.redirect('/')
+  })
+
+  router.get('/edit/:id', (req, res) => {
+    let id = req.params.id
+    console.log(id);
+    db.collection('siswa').findOne({
+      "_id": ObjectID(id)
+
+    }, (err, result) => {
+      if (err) res.json(err)
+      res.status(200).render('edit', {
+        data: result
+      })
+    })
+  })
+
+  router.post("/edit/:id", (req, res) => {
+    let id = req.params.id
+    let insert = req.body
+    db.collection('siswa').updateOne({
+      _id: ObjectID(id)
+    }, {
+      $set: {
+
+        "string": insert.string,
+        "integer": insert.integer,
+        "float": insert.float,
+        "date": insert.date,
+        "boolean": insert.boolean
+      }
+    }, err => {
+      if (err) res.json(err)
+    })
+    res.redirect('/')
+  })
+
+
+
+
+
+
+  return router;
+}
